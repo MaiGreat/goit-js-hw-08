ad8b79a5567cc0ac5f6e0498aa2501370f8bcfde
Olha Maistrenko <olga.maistrenko@outlook.com>
2023-03-31 07:15:15 +0200

    task01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,38 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const createHtmlLi = createGalleryImage(galleryItems);
+
+console.log(gallery)
+
+function createGalleryImage(images) {
+    return images
+        .map(({ preview, original, description }) => {
+            return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image"
+            src="${preview}" 
+            alt="${description}" />
+        </a>
+    </li>`
+        }
+    )
+    .join('');
+}
+
+gallery.insertAdjacentHTML('afterbegin', createHtmlLi);
+
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    
+    captionsData: 'alt',
+    captionDelay: 250,
+ });
