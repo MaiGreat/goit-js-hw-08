842c729853cbf80efea6a90e3a65390f71673c4d
Olha Maistrenko <olga.maistrenko@outlook.com>
2023-04-02 17:53:28 +0200

    task03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -32,8 +32,3 @@ player.setCurrentTime(currentTime).then(function(seconds) {
     }
 });
 
-// const onPlay = function(data) {
-//     // data is an object containing properties specific to that event
-// };
-
-// player.on('play', onPlay);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from "lodash.throttle";
 
+const KEYFEEDBACK = "feedback-form-state";
+
+const formaData = {};
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('textarea'),
+    // input: document.querySelector('iput')
+}
+
+console.log(refs);
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextInput, 500));
+textarea()
+
+refs.form.addEventListener('input', evt => {
+    formaData[evt.target.name] = evt.target.value;
+    console.log(formaData)
+})
+
+function onTextInput(event) {
+    const text = event.target.value;
+    console.log(text);
+
+    localStorage.setItem(KEYFEEDBACK, text)
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.target.reset();
+    localStorage.removeItem(KEYFEEDBACK)
+}
+
+function textarea() {
+    const saveText = localStorage.getItem(KEYFEEDBACK);
+    if (saveText) {
+        console.log(saveText);
+        refs.textarea.value = saveText;
+    }
+    
+}
